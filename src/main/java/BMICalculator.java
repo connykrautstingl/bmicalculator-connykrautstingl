df0b4e9412fbe0cd683f3a53f9ec5ebb6999bb9c
connykrautstingl <s44784@edu.campus02.at>
2021-11-06 12:29:49 +0100

    change BMICategory

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -68,25 +68,22 @@ public class BMICalculator {
 
     public int calculateBMICategory() {
         // TODO: Your implementation
-        if ((calculateBMI() < 16.0 && getGender() == 'm')) ||(calculateBMI() < 15.0 && getGender() == 'w') {
+        if ((calculateBMI() < 16.0 && getGender() == 'm') || (calculateBMI() < 15.0 && getGender() == 'w')) {
             return -2;
-        }
-
-          else if ((calculateBMI() <= 16.0 && calculateBMI() >= 18.4) && getGender() == 'm') ||
-        ((calculateBMI() <= 15.0 && calculateBMI() >= 17.4) && getGender() == 'w') {
+        } else if ((calculateBMI() >= 16.0 && calculateBMI() <= 18.4 && getGender() == 'm') ||
+                ((calculateBMI() >= 15.0 && calculateBMI() <= 17.4 && getGender() == 'w')) {
             return -1;
         }
 
-            else if ((calculateBMI() <= 18.5 && calculateBMI() >= 24.9) && getGender() == 'm') ||
-        ((calculateBMI() <= 17.5 && calculateBMI() >= 23.9) && getGender() == 'w') {
+            else if ((calculateBMI() >= 18.5 && calculateBMI() <= 24.9) && getGender() == 'm') ||
+        ((calculateBMI() >= 17.5 && calculateBMI() <= 23.9) && getGender() == 'w') {
             return 0;
         }
 
-              else if ((calculateBMI() <= 25.0 && calculateBMI() >= 34.9) && getGender() == 'm') ||
-        ((calculateBMI() <= 24.0 && calculateBMI() >= 33.9) || getGender() == 'w') {
+              else if ((calculateBMI() >= 25.0 && calculateBMI() <= 34.9) && getGender() == 'm') ||
+        ((calculateBMI() >= 24.0 && calculateBMI() <= 33.9) || getGender() == 'w') {
             return 1;
         }
-
                             else if ((calculateBMI() >= 35.0 && getGender() == 'm')) ||
         ((calculateBMI() >= 34.0 && getGender() == 'w')) {
             return 2;
